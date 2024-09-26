4be9f4992336193cd6dc2fa7014bc5782ddf6249
Pablopanoli <est.pablo.quiroga@unimilitar.edu.co>
2024-09-26 16:35:23 -0500

    principio del codigo

---
+++ AC-26SEP/script.js
@@ -0,0 +1,211 @@
+class SVGManager {
+    constructor(svgElement) {
+        this.svgElement = svgElement;
+    }
+
+    crearElemento(tag, attributes) {
+        const element = document.createElementNS("http://www.w3.org/2000/svg", tag);
+        for (const [key, value] of Object.entries(attributes)) {
+            element.setAttribute(key, value);
+        }
+        return element;
+    }
+
+    agregarElemento(element) {
+        this.svgElement.appendChild(element);
+    }
+}
+
+class Punto {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Line {
+    constructor(manager, punto1, punto2) {
+        this.manager = manager;
+        this.x1 = punto1.x;
+        this.y1 = punto1.y;
+        this.x2 = punto2.x;
+        this.y2 = punto2.y;
+    }
+
+    bresenham() {
+        const points = [];
+        let x1 = this.x1, y1 = this.y1;
+        const x2 = this.x2, y2 = this.y2;
+
+        const dx = Math.abs(x2 - x1);
+        const dy = Math.abs(y2 - y1);
+        const sx = (x1 < x2) ? 1 : -1;
+        const sy = (y1 < y2) ? 1 : -1;
+        let err = dx - dy;
+
+        while (true) {
+            points.push({ x: x1, y: y1 });
+            if (x1 === x2 && y1 === y2) break;
+            const err2 = err * 2;
+            if (err2 > -dy) {
+                err -= dy;
+                x1 += sx;
+            }
+            if (err2 < dx) {
+                err += dx;
+                y1 += sy;
+            }
+        }
+        return points;
+    }
+
+    dibujar() {
+        const puntos = this.bresenham();
+        for (const { x, y } of puntos) {
+            const punto = this.manager.crearElemento('circle', {
+                cx: x,
+                cy: y,
+                r: 1,
+                fill: 'black'
+            });
+            this.manager.agregarElemento(punto);
+        }
+    }
+}
+
+class Circle {
+    constructor(manager, centro, r) {
+        this.manager = manager;
+        this.cx = centro.x;
+        this.cy = centro.y;
+        this.r = r;
+    }
+
+    bresenham() {
+        const points = [];
+        let x = this.r;
+        let y = 0;
+        let p = 1 - this.r;
+
+        while (x > y) {
+            points.push({ x: this.cx + x, y: this.cy + y });
+            points.push({ x: this.cx - x, y: this.cy + y });
+            points.push({ x: this.cx + x, y: this.cy - y });
+            points.push({ x: this.cx - x, y: this.cy - y });
+            points.push({ x: this.cx + y, y: this.cy + x });
+            points.push({ x: this.cx - y, y: this.cy + x });
+            points.push({ x: this.cx + y, y: this.cy - x });
+            points.push({ x: this.cx - y, y: this.cy - x });
+            y++;
+
+            if (p <= 0) {
+                p = p + 2 * y + 1;
+            } else {
+                x--;
+                p = p + 2 * y - 2 * x + 1;
+            }
+        }
+        return points;
+    }
+
+    dibujar() {
+        const puntos = this.bresenham();
+        for (const { x, y } of puntos) {
+            const punto = this.manager.crearElemento('circle', {
+                cx: x,
+                cy: y,
+                r: 1,
+                fill: 'black'
+            });
+            this.manager.agregarElemento(punto);
+        }
+    }
+}
+
+class Ellipse {
+    constructor(manager, centro, rx, ry) {
+        this.manager = manager;
+        this.cx = centro.x;
+        this.cy = centro.y;
+        this.rx = rx;
+        this.ry = ry;
+    }
+
+    bresenham() {
+        const points = [];
+        let x = 0;
+        let y = this.ry;
+        let rx2 = this.rx * this.rx;
+        let ry2 = this.ry * this.ry;
+        let p;
+
+        // Región 1
+        p = Math.round(ry2 - (rx2 * this.ry) + (0.25 * rx2));
+        while ((2 * ry2 * x) < (2 * rx2 * y)) {
+            points.push({ x: this.cx + x, y: this.cy + y });
+            points.push({ x: this.cx - x, y: this.cy + y });
+            points.push({ x: this.cx + x, y: this.cy - y });
+            points.push({ x: this.cx - x, y: this.cy - y });
+            x++;
+
+            if (p < 0) {
+                p += 2 * ry2 * x + ry2;
+            } else {
+                y--;
+                p += 2 * ry2 * x - 2 * rx2 * y + ry2;
+            }
+        }
+
+        // Región 2
+        p = Math.round(ry2 * (x + 0.5) * (x + 0.5) + rx2 * (y - 1) * (y - 1) - rx2 * ry2);
+        while (y > 0) {
+            points.push({ x: this.cx + x, y: this.cy + y });
+            points.push({ x: this.cx - x, y: this.cy + y });
+            points.push({ x: this.cx + x, y: this.cy - y });
+            points.push({ x: this.cx - x, y: this.cy - y });
+            y--;
+
+            if (p > 0) {
+                p -= 2 * rx2 * y + rx2;
+            } else {
+                x++;
+                p += 2 * ry2 * x - 2 * rx2 * y + rx2;
+            }
+        }
+        return points;
+    }
+
+    dibujar() {
+        const puntos = this.bresenham();
+        for (const { x, y } of puntos) {
+            const punto = this.manager.crearElemento('circle', {
+                cx: x,
+                cy: y,
+                r: 1,
+                fill: 'black'
+            });
+            this.manager.agregarElemento(punto);
+        }
+    }
+}
+
+// Crear el SVG y la instancia de SVGManager
+const svgCanvas = document.getElementById('svgCanvas');
+const svgManager = new SVGManager(svgCanvas);
+
+// Crear instancias de Punto
+const punto1 = new Punto(50, 50);
+const punto2 = new Punto(200, 200);
+const centroCirculo = new Punto(300, 100);
+const centroElipse = new Punto(400, 300);
+
+// Dibujar las primitivas
+const linea = new Line(svgManager, punto1, punto2);
+linea.dibujar();
+
+const circunferencia = new Circle(svgManager, centroCirculo, 50);
+circunferencia.dibujar();
+
+const elipse = new Ellipse(svgManager, centroElipse, 80, 50);
+elipse.dibujar();
+
